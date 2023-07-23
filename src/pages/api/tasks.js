03a532d0f463f36c6e30cf3d35fdc096a07474a2
MehdiDgrs <mehdigital@outlook.com>
2023-07-23 21:29:48 +0200

    removed some stuff

--- src/pages/api/tasks.js
+++ src/pages/api/tasks.js
@@ -1,33 +1,34 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose'
 
-const { Schema } = mongoose;
-
+const { Schema } = mongoose
+let mdp = process.env.BDD_PASSWORD
+console.log(mdp)
 const taskSchemas = new Schema({
   title: String, // String is shorthand for {type: String}
   author: String,
-});
-let Task;
+})
+let Task
 
 if (mongoose.models.Task) {
-  Task = mongoose.model("Task");
+  Task = mongoose.model('Task')
 } else {
-  Task = mongoose.model("Task", taskSchemas);
+  Task = mongoose.model('Task', taskSchemas)
 }
 
 export default async function handler(req, res) {
   try {
     await mongoose.connect(
-      "mongodb+srv://obelisque59:zqcG6NASghoWkHJS@cluster0.guauuz0.mongodb.net/?retryWrites=true&w=majority"
-    );
+      `mongodb+srv://obelisque59:${mdp}@cluster0.guauuz0.mongodb.net/?retryWrites=true&w=majority`
+    )
   } catch (error) {
-    console.log(error);
-    return res.status(400).json({ message: "fail" });
+    console.log(error)
+    return res.status(400).json({ message: 'fail' })
   }
 
   const firstTask = new Task({
     title: " gagner de l'argent ",
-    author: "dimeh",
-  });
-  await firstTask.save();
-  res.status(200).json(firstTask);
+    author: 'dimeh',
+  })
+  await firstTask.save()
+  res.status(200).json(firstTask)
 }
